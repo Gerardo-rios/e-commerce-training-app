792ca6fbdd959484cba60c404036ab1743e06661
jsgerardor <gerardo.rios@ioet.com>
2024-04-01 17:00:09 -0500

    R1-Product-rate-from-api-implemented

--- src/components/ResultTable/Card/Detail/index.jsx
+++ src/components/ResultTable/Card/Detail/index.jsx
@@ -5,13 +5,14 @@ import './Detail.css'
 Detail.propTypes = {
     title: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired,
 };
 
-function Detail ({title, price }) {
+function Detail ({title, price, rate }) {
     return (
         <div className="DetailsCardContainer">
             <h3>{title}</h3>
-            <Rating stars={3}/>
+            <Rating stars={rate}/>
             <h3>${price}</h3>
         </div>
     )
