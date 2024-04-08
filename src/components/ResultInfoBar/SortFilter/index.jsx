b25c1af48c4e431eaf84fa41e7243c653b93c684
jsgerardor <gerardo.rios@ioet.com>
2024-04-08 13:58:44 -0500

    [r-4] Added filters by price and by name

--- src/components/ResultInfoBar/SortFilter/index.jsx
+++ src/components/ResultInfoBar/SortFilter/index.jsx
@@ -1,9 +1,21 @@
 import './SortFilter.css'
+import { useContext } from 'react';
+import { SearchContext } from '../../../contexts/SearchContext'
 
 function SortFilter () {
+
+    const {
+        filterByPriceValue,
+        setFilterByPriceValue,
+    } = useContext(SearchContext);
+
+    const handleSelectChange = (event) => {
+        setFilterByPriceValue(event.target.value);
+    }
+
     return (
         <div className='SortFilterContainer'>
-            <select name="order" id="order">
+            <select name="order" id="order" value={filterByPriceValue} onChange={handleSelectChange}>
                 <option value="Name">Name</option>
                 <option value="Price_Low">Price: Low to High</option>
                 <option value="Price_High">Price: High to Low</option>
